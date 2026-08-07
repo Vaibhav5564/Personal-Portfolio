08b53359d95aeeb7e28c7e043ad906b98513472e
Vaibhav Rajendra Adsul <vaibhavadsul5564@gmail.com>
2026-08-07 12:45:48 +0000

    Added a project in Personal Portfolio

--- src/data/projects.js
+++ src/data/projects.js
@@ -46,6 +46,36 @@ const projects = [
       "https://github.com/Vaibhav5564/React-Js/tree/main/my-app",
     live: "https://react-js-hi44.vercel.app",
     featured: false
+  },
+  {
+    id: 4,
+    title: "Crop Amount Calculator",
+    image: "/images/projects/crop.jpg",
+    description:
+      "Crop Amount Calculator is a web-based application built with HTML, CSS, JavaScript, PHP, and SQLite. It calculates crop area and amount based on user inputs, stores customer records securely, supports user authentication, password management, and provides an admin dashboard to monitor users, transactions, and overall business data efficiently.",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript"
+    ],
+    github: "https://github.com/Vaibhav5564/Projects-By-Using-HTML-CSS-and-JavaScript/tree/main/Crop%20Amount%20Calculator",
+    live: "https://crop-amount-calculator.onrender.com",
+    featured: true
+  },
+  {
+    id: 5,
+    title: "Currency Converter",
+    image: "/images/projects/currency-converter.png",
+    description:
+      "Currency Converter is a responsive web application built using HTML, CSS, and JavaScript. It enables users to convert amounts between different currencies using real-time exchange rates through an API. The application features an intuitive interface, instant conversion results, mobile-friendly design, and provides a fast, simple, and accurate currency conversion experience.",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript"
+    ],
+    github: "https://github.com/Vaibhav5564/Projects-By-Using-HTML-CSS-and-JavaScript/tree/main/Currency%20Converter",
+    live: "https://currency-converter-5564.netlify.app/",
+    featured: true
   }
 ];
 
